1db5be0c4aa1fb1d4f1b813fddb995fb4aaee7ad
Faizan Saifi <Faizansaifi571@gmail.com>
2025-12-16 11:40:09 +0530

    new_website

--- src/pages/admin/ProductLink/AddProduct.jsx
+++ src/pages/admin/ProductLink/AddProduct.jsx
@@ -17,28 +17,27 @@ import {
 
 import { Loader2 } from "lucide-react";
 
-import category_amazon from "../../../assets/BrandLogo/category_amazon.png";
-import category_flipkart from "../../../assets/BrandLogo/category_flipkart.png";
-// import category_ajio from "../../../assets/BrandLogo/category_ajio.png";
-import category_myntra from "../../../assets/BrandLogo/category_myntra.png";
-import category_ebay from "../../../assets/BrandLogo/category_ebay.png";
-import category_nykaa from "../../../assets/BrandLogo/category_nykaa.png";
-import category_Etsy from "../../../assets/BrandLogo/category_etsy.png";
-import category_meessho from "../../../assets/BrandLogo/category_meesho.png";
+import category_amazon from "@/assets/BrandLogo/category_amazon.png";
+import category_flipkart from "@/assets/BrandLogo/category_flipkart.png";
+import category_myntra from "@/assets/BrandLogo/category_myntra.png";
+// import category_ajio from "@/assets/BrandLogo/category_ajio.png";
+// import category_ebay from "@/assets/BrandLogo/category_ebay.png";
+// import category_nykaa from "@/assets/BrandLogo/category_nykaa.png";
+// import category_Etsy from "@/assets/BrandLogo/category_etsy.png";
+// import category_meessho from "@/assets/BrandLogo/category_meesho.png";
 
 // ------------------------------------
 // Website Config
 // ------------------------------------
-
 const WEBSITES = [
   { id: "amazon", logo: category_amazon },
   { id: "flipkart", logo: category_flipkart },
-  // { id: "ajio",  logo: category_ajio },
   { id: "myntra",  logo: category_myntra },
-  { id: "ebay",  logo: category_ebay },
-  { id: "nykaa",  logo: category_nykaa },
-  { id: "etsy",  logo: category_Etsy },
-  { id: "meesho", logo: category_meessho },
+  // { id: "ajio",  logo: category_ajio },
+  // { id: "ebay",  logo: category_ebay },
+  // { id: "nykaa",  logo: category_nykaa },
+  // { id: "etsy",  logo: category_Etsy },
+  // { id: "meesho", logo: category_meessho },
 ];
 
 // ------------------------------------
