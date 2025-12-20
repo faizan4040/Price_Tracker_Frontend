cf73c23af7ba7f986c3814e666572ab430b0f00c
Faizan Saifi <Faizansaifi571@gmail.com>
2025-12-20 19:03:26 +0530

    newwww

--- src/feature/api/api.js
+++ src/feature/api/api.js
@@ -1,17 +1,8 @@
 import axios from "axios";
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
-if (!API_BASE_URL) {
-  throw new Error(
-    "VITE_API_BASE_URL is missing! Set it in Vercel and redeploy."
-  );
-}
-
 const API = axios.create({
-  baseURL: API_BASE_URL,
-  withCredentials: true, // cookies
-  headers: { "Content-Type": "application/json" },
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
 });
 
 export default API;
@@ -20,14 +11,3 @@ export default API;
 
 
 
-
-
-
-// import axios from "axios";
-
-// const API = axios.create({
-//   baseURL: import.meta.env.VITE_API_BASE_URL,
-//   withCredentials: true,
-// });
-
-// export default API;
